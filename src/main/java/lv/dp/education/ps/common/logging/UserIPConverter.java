9e3ec2d537a119668d92eb74c0fc71fe2d726ed7
Deniss Prohorenko <denpromail@gmail.com>
2020-04-12 18:21:09 +0300

    added skeleton for country logging

---
+++ src/main/java/lv/dp/education/ps/common/logging/UserIPConverter.java
@@ -0,0 +1,14 @@
+package lv.dp.education.ps.common.logging;
+
+import ch.qos.logback.classic.pattern.ClassicConverter;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+public class UserIPConverter extends ClassicConverter {
+    @Override
+    public String convert(ILoggingEvent event) {
+        return ((ServletRequestAttributes)RequestContextHolder.currentRequestAttributes())
+                .getRequest().getRemoteAddr();
+    }
+}
---
+++ src/main/java/lv/dp/education/ps/configuration/RequestLoggingFilterConfig.java
@@ -0,0 +1,17 @@
+package lv.dp.education.ps.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@Configuration
+public class RequestLoggingFilterConfig {
+    @Bean
+    public CommonsRequestLoggingFilter logFilter() {
+        CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter();
+        filter.setIncludeQueryString(true);
+        filter.setIncludePayload(false);
+        filter.setIncludeHeaders(false);
+        return filter;
+    }
+}
